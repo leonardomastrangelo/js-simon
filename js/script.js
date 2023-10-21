0c8ed0c7c9ed3cface2ce943e3463b17a2436b1f
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-21 12:33:29 +0200

    reorganized in more functions

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function myApp() {
     /**
      * DATA
     */
+
     // random numbers to generate
     const maxRndNumbers = 5;
     // min
@@ -12,11 +13,11 @@ function myApp() {
     // max 
     const max = 100;
     // array where to push random generated numbers
-    const generatedRndNumbers = []
+    let generatedRndNumbers = []
     // array of numbers inserted in input by user
-    const insertedNumbers = []
+    let insertedNumbers = []
     // array of corrected selected number
-    const gotRight = []
+    let gotRight = []
     // timer for numbers' disappearance
     const timeToDisappeare = 3 * 1000;
     // caputing in variable the div in which i print the generated numbers
@@ -35,26 +36,35 @@ function myApp() {
     let gameOver = false;
     // taking restart btn
     const btnRestart = document.querySelector(".btn-info");
+
     /**
      * EXECUTION
     */
 
     // pushing 5 rnd numbers in array
-    while (generatedRndNumbers.length < maxRndNumbers) {
-        // singular rnd number
-        let rndNumber = getRndInteger(min, max);
-        if (!generatedRndNumbers.includes(rndNumber)) {
-            generatedRndNumbers.push(rndNumber)
+    pushRndToArray()
+    function pushRndToArray() {
+        while (generatedRndNumbers.length < maxRndNumbers) {
+            // singular rnd number
+            let rndNumber = getRndInteger(min, max);
+            if (!generatedRndNumbers.includes(rndNumber)) {
+                generatedRndNumbers.push(rndNumber)
+            }
         }
     }
 
     // printing rnd numbers in HTML
-    for (let i = 0; i < generatedRndNumbers.length; i++) {
-        const element = generatedRndNumbers[i];
-        rndNumPrint.innerHTML +=
+    printRndHtml()
+    function printRndHtml() {
+        for (let i = 0; i < generatedRndNumbers.length; i++) {
+            const element = generatedRndNumbers[i];
+            rndNumPrint.innerHTML +=
+                `
+            <div class="col-4 text-center display-2 py-5">${element}</div>
             `
-        <div class="col-4 text-center display-2 py-5">${element}</div>
-        `
+        }
+        // win/loss control
+        console.log(generatedRndNumbers);
     }
 
     // start game phase
@@ -62,10 +72,13 @@ function myApp() {
     function startGame() {
         rndNumPrint.classList.add("d-none")
         containerData.classList.remove("d-none")
+        containerData.classList.add("d-flex")
     }
 
-    // on click pushing inserted numbers in array
-    btn.addEventListener("click", pushUserNum, {once : true})
+    /**
+     * MAIN ALGORITHM OF MYAPP
+     */
+    btn.addEventListener("click", pushUserNum, { once: true })
     function pushUserNum() {
         // capturing variables from imput
         let num1 = document.getElementById("num-1").value;
@@ -76,51 +89,68 @@ function myApp() {
         insertedNumbers.push(num1, num2, num3, num4, num5)
 
         // checking if every num inserted by user is included in rnd number's array
-        for (let i = 0; i < generatedRndNumbers.length; i++) {
-            const element = insertedNumbers[i];
-            if (generatedRndNumbers.includes(parseInt(element))) {
-                score++
-                gotRight.push(element)
-            } else {
-                gameOver = true;
+        check()
+        function check() {
+            for (let i = 0; i < generatedRndNumbers.length; i++) {
+                const element = insertedNumbers[i];
+                if (generatedRndNumbers.includes(parseInt(element))) {
+                    score++
+                    gotRight.push(element)
+                } else {
+                    gameOver = true;
+                }
             }
         }
 
         // msg printing
-        msg = (gameOver === true) ? "Non ci sei riuscito del tutto" : "Ottima Memoria"
+        msgPrinting()
+        function msgPrinting() {
+            msg = (gameOver === true) ? "Ma ci vedi?" : "Ottima Memoria"
+        }
 
         // printing in console results
-        console.log(msg);
-        console.log("numeri da indovinare:",generatedRndNumbers);
-        console.log("numeri indovinati:",score);
-        console.log("quali numeri indovinati:",gotRight);
+        consoleResultPrint()
+        function consoleResultPrint() {
+            console.log(msg);
+            console.log("numeri da indovinare:", generatedRndNumbers);
+            console.log("numeri indovinati:", score);
+            console.log("quali numeri indovinati:", gotRight);
+        }
 
         // printing in HTML results
-        result.innerHTML = 
-        `
-        <div class="row d-flex justify-content-center align-items-center text-center">
-            <div class="col">
-                <h3 class="display-3 fw-bold py-4">${msg}</h3>
-                <div class="fs-2 fw-bold">Numeri da indovinare</div>
-                ${generatedRndNumbers}
-                <div class="fs-2 fw-bold mt-5">Score</div>
-                ${score}
-                <div class="fs-2 fw-bold mt-5 pb-4">Quali numeri hai indovinato</div>
-                ${gotRight}
+        htmlResultPrint()
+        function htmlResultPrint() {
+            if (gotRight.length === 0) {
+                gotRight.push("Non ci siamo proprio...")
+            }
+            result.innerHTML =
+                `
+            <div class="row d-flex justify-content-center align-items-center text-center py-5">
+                <div class="col">
+                    <h3 class="display-3 fw-bold pb-4">${msg}</h3>
+                    <div class="fs-2 fw-bold">Numeri da indovinare</div>
+                    ${generatedRndNumbers}
+                    <div class="fs-2 fw-bold mt-5">Score</div>
+                    ${score}
+                    <div class="fs-2 fw-bold mt-5">Quali numeri hai indovinato</div>
+                    ${gotRight}
+                </div>
             </div>
-        </div>
-        `
+            `
+        }
 
-        // // appearance restart button
-        // setTimeout(restartTimer, 3000)
-        // function restartTimer() {
-        //     btnRestart.classList.remove("d-none")
-        // }
-        // // restart the app
-        // btnRestart.addEventListener("click", ()=> {
-        //     myApp
+        // appearance restart button
+        setTimeout(restartTimer, 3000)
+        function restartTimer() {
+            btnRestart.classList.remove("d-none")
+        }
 
-        // })
+        // restart the app
+        btnRestart.addEventListener("click", restart)
+        function restart() {
+            location.reload()
+        }
     }
+
 
 }
